9990b8a896acec8fa9237b32d63189aca4d4f151
Aya Mahmoud Hassan <116037854+AyaAbdelghaffar211@users.noreply.github.com>
2024-08-01 17:48:24 +0300

    Cleanup Day5TaskApplicationTests.java

--- src/test/java/com/example/demo/Day5TaskApplicationTests.java
+++ src/test/java/com/example/demo/Day5TaskApplicationTests.java
@@ -1,12 +1,7 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class Day5TaskApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
