68ccad1cf62aae4f0004581cb9f296fec6e6310b
Aya Abdelghaffar <ayaabdelghaffar211@gmail.com>
2024-08-01 15:13:04 +0300

    use spring instead of spring boot

--- src/main/java/com/example/demo/Day5TaskApplication.java
+++ src/main/java/com/example/demo/Day5TaskApplication.java
@@ -1,15 +1,14 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Day5TaskApplication {
 
 	public static void main(String[] args) {
 
 		//test when using variable names to give a priority
-		ApplicationContext context = SpringApplication.run(ApplicationConfig.class);
+		ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
 		CourseService courseService = context.getBean(CourseService.class);
 		courseService.printRecommendedCourses();
 	}
