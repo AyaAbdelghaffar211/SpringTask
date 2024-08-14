549e2d586e154eb683fcee9aca3f7fef13db2ab6
Aya Abdelghaffar <ayaabdelghaffar211@gmail.com>
2024-08-14 16:08:04 +0300

    moved hardcoded username and password to app.properties

--- src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,10 +11,21 @@ import java.util.ArrayList;
 
 @Component
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Value("${spring.security.user.name}")
+    private String username;
+
+    @Value("${spring.security.user.password}")
+    private String password;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return User.withUsername("user")
-                .password("user")
+        if (!this.username.equals(username)) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return User.withUsername(username)
+                .password(password)
                 .authorities(new ArrayList<>())
                 .accountExpired(false)
                 .accountLocked(false)
